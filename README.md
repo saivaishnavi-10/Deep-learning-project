d9d2c5af2a5a1684388bbc8f692c7093dbca7619
saivaishnavi-10 <santhapurisaivaishnavi@gmail.com>
2025-11-13 10:54:14 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Deep-learning-project
+# project workflow  
+update config.yaml
+update parms.yaml
+update entity
+update config manager in src config 
+update the components
+update pipeline
+update main.py
+update dvc.yaml file 
--- templates.py
+++ templates.py
@@ -22,8 +22,8 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html"
-
+    "templates/index.html" 
+                                                                                                                                                                                                                                                                                                                                                                                                                                                  
 
 ]
 
